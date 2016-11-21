78ad43835297e59fe391fe0716ccb92cddb3d7eb
Theodore <tfjtan@hotmail.com>
2016-11-21 18:24:55 -0500

    probably wong, but gonna have to work on it later

--- Python3/functions.py
+++ Python3/functions.py
@@ -1,5 +1,4 @@
 from random import randint
-import time
 
 class CSP():
     """
@@ -30,12 +29,34 @@ def get_num_conflicts(queen, queens):
     def neg(x, y, i): return -i+(y+x)
     x, y, count = queen[0], queen[1], 0
 
-    for q in queens:
-        x1, y1 = queens[q][0], queens[q][1]
-        if (queen != queens[q]) and ((y == y1) or (y1 == pos(x, y, x1) or
+    for _, value in queens.items():
+        x1, y1 = value[0], value[1]
+        if (queen != value) and ((y == y1) or (y1 == pos(x, y, x1) or
             (y1 == neg(x, y, x1)))):
             count += 1
     return count
+
+
+def get_conflicts(queen, queens, conflict_list=None):
+    def pos(x, y, i): return i+(y-x)
+    def neg(x, y, i): return -i+(y+x)
+    x, y, conflicts = queen[0], queen[1], []
+
+    if not conflict_list:
+        for key, value in queens.items():
+            x1, y1 = value[0], value[1]
+            if (queen != value) and ((y == y1) or (y1 == pos(x, y, x1) or
+                (y1 == neg(x, y, x1)))):
+                conflicts.append(key)
+    else:
+        for key in conflict_list:
+            value = queens[key]
+            x1, y1 = value[0], value[1]
+            if (queen != value) and ((y == y1) or (y1 == pos(x, y, x1) or
+                (y1 == neg(x, y, x1)))):
+                conflicts.append(key)
+
+    return conflicts
 
 
 def min_conflicts(csp, max_steps):
@@ -55,45 +76,43 @@ def min_conflicts(csp, max_steps):
     # current = initial complete assignment for csp
     # pass in csp as a complete inital assignment
     past_var = []
-    start_time = time.time()
+
     for i in range(1, max_steps+1):
         # print(csp.domains)
         # print(csp.constraints)
-        conflicted = [i for i in csp.variables if csp.constraints[i] != 0]
-        if not any(conflicted):
+        conflicted = [i for i, j in csp.constraints.items() if len(j) != 0]
+        if conflicted == []:
             print(i)
-            end = (time.time() - start_time)
-            print("{}".format(end))
             return csp
         # print(get_min_conflicts(conflicted, csp))
         var = conflicted[randint(0, len(conflicted)-1)]
 
         # var = get_min_conflicts(conflicted, csp)
-        value= conflicts(var, csp)
+        value, count= conflicts(var, csp)
         if past_var is not []:
-            if len(past_var) >= 50: past_var.pop(0)
+            if len(past_var) >= 100: past_var.pop(0)
             while (var, value) in past_var:
                 var = conflicted[randint(0, len(conflicted)-1)]
-                value = conflicts(var, csp)
+                value, count = conflicts(var, csp)
         else: past_var.append((var, value))
         csp.domains[var] = [int(var[1:]), value]
+        # if count > 0:
         update_conflicts(csp)
-        #csp.constraints[var] = count
-    end = (time.time() - start_time)
-    print("{}".format(end))
+        # else:
+        #     csp.constraints[var] = count
     return False
 
 
 def update_conflicts(csp):
     for i in csp.variables:
-        csp.constraints[i] = get_num_conflicts(csp.domains[i], csp.domains)
+        csp.constraints[i] = get_conflicts(csp.domains[i], csp.domains, csp.constraints[i])
     return
 
 
 def get_min_conflicts(conflicted, csp):
     least = conflicted[0]
     for x in conflicted:
-        if csp.constraints[least] > csp.constraints[x]:
+        if len(csp.constraints[least]) > len(csp.constraints[x]):
             least = x
     return least
 
@@ -102,10 +121,10 @@ def conflicts(var, csp):
     column, asdf = {}, []
     x = int(var[1:])
     min_column = 1 if 1 != csp.domains[var][1] else 2
-    min_count = get_num_conflicts([x, min_column], csp.domains)
+    min_count = len(get_conflicts([x, min_column], csp.domains))
     for y in range(1, len(csp.variables)+1):
         if y != csp.domains[var][1]:
-            count = get_num_conflicts([x, y], csp.domains)
+            count = len(get_conflicts([x, y], csp.domains))
             if count < min_count:
                 column = {}
                 column[y] = count
@@ -120,8 +139,8 @@ def conflicts(var, csp):
         min_count = column[asdf[rand]]
         min_column = asdf[rand]
 
-    return min_column
-
+    # print(min_count)
+    return min_column, min_count
 
 
 def create_board(n):
--- Python3/main.py
+++ Python3/main.py
@@ -1,31 +1,29 @@
-import sys
-from functions import CSP, get_num_conflicts, min_conflicts, create_board, print_board
+import sys, time
+from functions import CSP, get_conflicts, min_conflicts, create_board, print_board
 from random import randint
 
 n = int(sys.argv[1])
 
-# board = create_board(n)
 variables = ['Q{}'.format(i) for i in range(1, n+1)]
-# print(variables)
-
 domains = {key : [int(key[1:]), randint(1, n)] for key in variables}
-# print(domains)
 
-# domains = {'Q1': [1, 2], 'Q2': [2, 4], 'Q3': [3, 1], 'Q4': [4, 3]}
-
-# constraints = {}
-constraints = {key: get_num_conflicts(domains[key], domains) for key in variables}
-# print(constraints)
+constraints = {key: get_conflicts(domains[key], domains) for key in variables}
 
 print()
 csp = CSP(variables, domains, constraints)
 
+start_time = time.time()
 assignment = min_conflicts(csp, max_steps=1000)
+end_time = time.time()
 
-b = create_board(n)
-for key, value in assignment.domains.items():
-    b[value[1] - 1][value[0] - 1] = 'Q'
-print_board(b)
+print(end_time - start_time)
+print()
 
 if not assignment:
     print(assignment)
+else:
+    b = create_board(n)
+    for key, value in assignment.domains.items():
+        b[value[1] - 1][value[0] - 1] = 'Q'
+    print_board(b)
+    # pass
