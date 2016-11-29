8f82606ab96ecab1053b320a174050daa2bdb3a5
Theodore <tfjtan@hotmail.com>
2016-11-29 18:16:36 -0500

    reverted back to original master

--- Python3/main.py
+++ Python3/main.py
@@ -1,6 +1,6 @@
 import sys, time
 from argparse import ArgumentParser
-from functions import CSP, get_conflicts, min_conflicts, create_board, print_board, get_least_conflicts_y, get_empty_spot
+from functions import CSP, get_conflicts, min_conflicts, create_board, print_board, get_least_conflicts_y
 from random import randint
 
 parser = ArgumentParser(description="A N-Queens Solver")
@@ -15,17 +15,9 @@ variables = ['Q{}'.format(i) for i in range(1, n+1)]
 # domains = {key : [int(key[1:]), randint(1, n)] for key in variables}
 domains = {}
 for i in range(1, n+1):
-    # if (i < 200):
-        domains['Q{}'.format(i)] = get_least_conflicts_y(i, n, domains)
-    # else:
-    #     domains['Q{}'.format(i)] = i
+    domains['Q{}'.format(i)] = [i, get_least_conflicts_y(i, n, domains)]
 
-constraints = {key: get_conflicts([int(key[1:]), domains[key]], domains) for key in variables}
-
-# Setup Time
-print('Set-up Time: {:0.5f} secs'.format(time.time() - start_time))
-
-
+constraints = {key: get_conflicts(domains[key], domains) for key in variables}
 
 csp = CSP(variables, domains, constraints)
 
@@ -33,7 +25,7 @@ csp = CSP(variables, domains, constraints)
 # print('Initial')
 # b = create_board(n)
 # for key, value in csp.domains.items():
-#     b[value - 1][int(key[1:]) - 1] = 'Q'
+#     b[value[1] - 1][int(key[1:]) - 1] = 'Q'
 # print_board(b)
 # print()
 
@@ -49,5 +41,5 @@ if assignment: print('Time: {:0.5f} secs'.format(time.time() - start_time))
 #     print('Complete')
 #     b = create_board(n)
 #     for key, value in assignment.domains.items():
-#         b[value - 1][int(key[1:]) - 1] = 'Q'
+#         b[value[1] - 1][int(key[1:]) - 1] = 'Q'
 #     print_board(b)
